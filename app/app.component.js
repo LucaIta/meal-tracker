64bcbfe4ebec7898b937cb533e39fe69e82ed532
Luca Quatela <lucaqq@gmail.com>
2016-06-02 21:32:00 -0700

    add meal class and meal-display component

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './meals.display.component', './meal.model.ts'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,21 +10,33 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, meals_display_component_1, meal_model_ts_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (meals_display_component_1_1) {
+                meals_display_component_1 = meals_display_component_1_1;
+            },
+            function (meal_model_ts_1_1) {
+                meal_model_ts_1 = meal_model_ts_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.mealsList = [
+                        new meal_model_ts_1.Meal("Breakfast", "This is breakfast", 100),
+                        new meal_model_ts_1.Meal("Lunch", "This is Lunch", 200),
+                        new meal_model_ts_1.Meal("Dinner", "This is Dinner", 300),
+                    ];
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n   <div class=\"container\">\n     <h1>Meal Tracker</h1>\n   </div>\n  "
+                        directives: [meals_display_component_1.MealsDisplayComponent],
+                        template: "\n   <div class=\"container\">\n     <h1>Meal Tracker</h1>\n     <meals-display [meals]=\"mealsList\"></<meals-display>>\n   </div>\n  "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
