7007dd6a5b5644e54f44ef9bbadb93bf70e7280a
Luca Quatela <lucaqq@gmail.com>
2016-06-02 20:45:00 -0700

    will add the meal class modeule

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,1 +1,14 @@
+import { Component } from 'angular2/core';
 
+@Component({ //component decorator defining selector and template
+  selector: 'my-app',
+  template: `
+   <div class="container">
+     <h1>Meal Tracker</h1>
+   </div>
+  `
+})
+
+export class AppComponent {
+
+}
